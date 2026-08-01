a6f0ac167bdd8ba2112552d3c58b3610509ad7c7
Ignacio Pinto <Pinto2291@users.noreply.github.com>
2026-08-01 12:42:19 -0400

    archivos adicionales

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // REEMPLAZAR 'valery-portfolio' CON EL NOMBRE EXACTO DE TU REPOSITORIO EN GITHUB
+  // Esto es crucial para que los assets (css, js, imágenes) carguen correctamente en GH Pages
+  base: '/https://github.com/Pinto2291/valery-portafolio/', 
 })
